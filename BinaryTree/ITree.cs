7ea5fde2a0b30a13a228029288c4fdb7322728c9
Dmitriy Filimonov <banodannon@gmail.com>
2021-01-25 20:47:32 +0300

    start

---
+++ BinaryTree/ITree.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryTree
+{
+    public interface ITree
+    {
+        void Add(int value);
+        void Delete(int value);
+        void Read();
+        void GetElementByValue(int value);
+        void GetMax();
+        void GetMin();
+        void GetFirstWhereMaxCount();
+        void GetFirstWhereMinCount();
+        void CountAllAndSum();
+    }
+}
---
+++ BinaryTree/Node.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryTree
+{
+    public class Node
+    {
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+        public int Value { get; set; }
+        public int Count { get; set; }
+    }
+}
